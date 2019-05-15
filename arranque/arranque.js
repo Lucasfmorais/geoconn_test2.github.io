aa3f1025cd9bdfc4eec07404a9ac35cf25546fdd
Lucasfmorais <lucasengflorestal@gmail.com>
2019-05-15 16:17:18 -0300

    ver1

--- arranque/arranque.js
+++ arranque/arranque.js
@@ -1,11 +1,34 @@
 var map = new ol.Map({
-    target: 'map',
-    layers: layersList,
-    view: new ol.View({
-      center: ol.proj.fromLonLat([-51.7257015,-17.8869303]),
-      zoom: 15
-    })
-  });
+	layers: [google, lyr_EsriSatellite_1],
+	target: 'map',
+	
+	controls: ol.control.defaults({
+		attributionOptions: /** @type {olx.control.AttributionOptions} */ ({
+			collapsible: true,
+		})
+	}),
+	view: new ol.View({
+		center: [-5758082.04,-2024314.37],
+		zoom: 15
+	})
+});
+
+var swipe = document.getElementById('swipe');
+lyr_EsriSatellite_1.on('precompose', function(event) {
+	var ctx = event.context;
+	var width = ctx.canvas.width * (swipe.value / 100);
+	ctx.save();
+	ctx.beginPath();
+	ctx.rect(width, 0, ctx.canvas.width - width, ctx.canvas.height);
+	ctx.clip();
+});
+lyr_EsriSatellite_1.on('postcompose', function(event) {
+	var ctx = event.context;
+	ctx.restore();
+});
+swipe.addEventListener('input', function() {
+	map.render();
+}, true);
 
 var vector = new ol.layer.Vector({
   source: new ol.source.Vector({
--- layers/layers.js
+++ layers/layers.js
@@ -1,4 +1,4 @@
-   var google = new ol.layer.Tile({
+var google = new ol.layer.Tile({
 	'title': 'Google Satellite',
 	'type': 'base',
 	'opacity': 1.00000,
@@ -9,8 +9,7 @@
 		url: 'https://mt1.google.com/vt/lyrs=s&x={x}&y={y}&z={z}'
 	})
 });
-
-var lyr_geoconn2 = new ol.layer.Tile({   
+var lyr_geoconn2_2 = new ol.layer.Tile({   
 	'title': 'geoconn2',
 	'type': 'base',
 	'opacity': 1.000000,
@@ -19,8 +18,7 @@ var lyr_geoconn2 = new ol.layer.Tile({
 		url: 'https://storage.googleapis.com/land_test/Test_landSAt/{z}/{x}/{y}.jpg'
 	})
 });
-
-var lyr_geconn1 = new ol.layer.Tile({
+var lyr_geconn1_1 = new ol.layer.Tile({
 	'title': 'geconn1',
 	'type': 'base',
 	'opacity': 1.000000,
@@ -28,8 +26,7 @@ var lyr_geconn1 = new ol.layer.Tile({
 		url: 'https://storage.googleapis.com/wgs_test/Test_wgs_qgis/{z}/{x}/{y}.jpg'
 	})
 });
-
-var lyr_geoconn3 = new ol.layer.Tile({
+var lyr_geoconn3_3 = new ol.layer.Tile({
 	'title': 'geoconn3',
 	'type': 'base',
 	'opacity': 1.000000,
@@ -37,8 +34,7 @@ var lyr_geoconn3 = new ol.layer.Tile({
 		url: 'https://storage.googleapis.com/wgs_test/Test_wgs_qgis2/{z}/{x}/{y}.jpg'
 	})
 });
-
-var lyr_EsriSatellite = new ol.layer.Tile({
+var lyr_EsriSatellite_1 = new ol.layer.Tile({
 	'title': 'Esri Satellite',
 	'type': 'base',
 	'opacity': 1.000000,
@@ -46,25 +42,3 @@ var lyr_EsriSatellite = new ol.layer.Tile({
 		url: 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}'
 	})
 });
-
-var format_bairrosjti_3 = new ol.format.GeoJSON();
-
-var features_bairrosjti_3 = format_bairrosjti_3.readFeatures(json_bairrosjti_3, 
-{dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
-
-var jsonSource_bairrosjti_3 = new ol.source.Vector({attributions: '<a href=""></a>'});
-
-jsonSource_bairrosjti_3.addFeatures(features_bairrosjti_3);
-
-var lyr_bairrosjti_3 = new ol.layer.Vector({
-	declutter: true,
-	source:jsonSource_bairrosjti_3, 
-	style: style_bairrosjti_3,
-});
-
-/* --------------configura do menu lateral ----------------*/
-
-/* lyr_bairrosjti_3.setVisible(true); */
-
-var layersList = [google,
-];
